a96e6b0ebd591d770b291d0fd7d6c52f2f260b87
e2thenegpii <e2thenegpii@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2010-12-11 22:27:47 +0000

    The new location for the sensor code.  Intended to be put in a static library for easier access to all the different sensors now that we are starting integration. Eldon

--- MultiSensorLib/Debug/objects.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lmrpt-detectors -lmrpt-hmtslam -lmrpt-hwdrivers -lmrpt-gui -lmrpt-reactivenav -lmrpt-slam -lmrpt-bayes -lmrpt-maps -lmrpt-topography -lmrpt-scanmatching -lmrpt-vision -lmrpt-obs -lmrpt-opengl -lmrpt-base
