18a1fb55e4ff3617f122f1ad4465bdc7dd6ee101
e2thenegpii <e2thenegpii@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2010-12-12 01:17:36 +0000

    Implemented a couple of commands for the motor controller, and documented the code a bit.  Eldon

--- branches/Sensors/MultiSensorLib/inc/MotorController.h
+++ branches/Sensors/MultiSensorLib/inc/MotorController.h
@@ -8,35 +8,100 @@
 #ifndef MOTORCONTROLLER_H_
 #define MOTORCONTROLLER_H_
 
+#include <string>
+
 #include <mrpt/hwdrivers/CSerialPort.h>
 
 class MotorController {
 public:
+	static const int MOTOR_SECRET_KEY = 321654987;//Motor reset key used for sensitive operations
 	enum MotorChannel{
 		Channel1 = 1,
 		Channel2,
 		BothChannels
 	};
 
-	MotorController( int motor1Max = 1000, int motor2Max = 1000,
-			int motor1Min = -1000, int motor2Min = -1000 ):
-		motor1Speed(0), motor2Speed(0),
-		motor1SpeedMax(motor1Max), motor2SpeedMax(motor2Max),
-		motor1SpeedMin(motor1Min), motor2SpeedMin(motor2Min) { };
+	MotorController( std::string portName, bool enableEcho = true, int motor1Max = 1000, int motor2Max = 1000,
+			int motor1Min = -1000, int motor2Min = -1000 );
 
 	virtual ~MotorController() { };
 
+	/**
+	 * setSpeed - sets the speed for the motor control channel
+	 * @param channel - the channel to change the speed { Channel1, Channel2, BothChannels }
+	 * @param value - the speed at which to set the channel
+	 * @return true upon success
+	 */
 	bool setSpeed( MotorChannel channel, int value );
-	void doProcess();
-private:
-	mrpt::hwdrivers::CSerialPort serialPort;
-	int motor1Speed;
-	int motor2Speed;
-	int motor1SpeedMax;
-	int motor2SpeedMax;
-	int motor1SpeedMin;
-	int motor2SpeedMin;
 
+	/**
+	 * doProcess - sends the current speed command to the motor controller
+	 */
+	void doProcess();
+
+	/**
+	 * emergencyStop - sends the emergency stop command to the motor controller
+	 * the motor controller will not start until it is power cycled or the clearEmergencyStop
+	 * function is called.
+	 * @returns true upon successful transmission
+	 */
+	bool emergencyStop();
+
+	/**
+	 * clearEmergencyStop - clears the emergency stop state
+	 * @returns true upon successful transmission
+	 */
+	bool clearEmergencyStop();
+
+	/**
+	 * reset - power cycles the motor controller
+	 * @returns true upon successful transmission
+	 */
+	bool reset();
+
+	/**
+	 * setTime - sets the local time of the motor controller to the time in the parameters
+	 * @param hours - the hours segment of the time
+	 * @param minutes - the minutes segment of the time
+	 * @param seconds - the seconds segment of the time
+	 * @returns true upon successful transmission
+	 */
+	bool setTime( int hours, int minutes, int seconds );
+
+private:
+	mrpt::hwdrivers::CSerialPort serialPort;//! the serial port to communicate to the motor controller
+	bool echoEnabled;						//! true if the motor controller is set to echo serial commands
+	int motor1Speed;						//! the current speed for the first channel motor
+	int motor2Speed;						//! the current speed for the second channel motor
+	const int motor1SpeedMax;				//! The hard upper limit for the first channel motor speed
+	const int motor2SpeedMax;				//! The hard upper limit for the second channel motor speed
+	const int motor1SpeedMin;				//! The hard lower limit for the first channel motor speed (lower limit would be the reverse speed)
+	const int motor2SpeedMin;				//! The hard lower limit for the second channel motor speed
+
+	/**
+	 * assertValidMotorRange - checks the parameters if they are in a valid range
+	 * @param max - the max value for the range
+	 * @param min - the min value for the range
+	 * @param value - the value to check
+	 * @returns true if the value is in range
+	 */
 	bool assertValidMotorRange( int max, int min, int value ) const;
+
+	/**
+	 * sendCommand - low level function call to send a command to the motor controller
+	 * @param command - the carriage return ended command to send to the motor controller
+	 * @returns true upon successful transmission of the command
+	 */
+	bool sendCommand( std::string command );
+
+	/**
+	 * enableSerialEcho - turns on the echo of commands from the motor controller
+	 */
+	void enableSerialEcho();
+
+	/**
+	 * disableSerialEcho - turns off the echo of commands from the motor controller
+	 */
+	void disableSerialEcho();
 };
 #endif /* MOTORCONTROLLER_H_ */
--- branches/Sensors/MultiSensorLib/src/MotorController.cpp
+++ branches/Sensors/MultiSensorLib/src/MotorController.cpp
@@ -6,10 +6,26 @@
  */
 
 #include <iostream>
+#include <sstream>
+#include <string>
 
 #include <MotorController.h>
 
 using namespace std;
+
+MotorController::MotorController( string portName, bool enableEcho, int motor1Max, int motor2Max, int motor1Min, int motor2Min ):
+		serialPort(portName), echoEnabled(enableEcho),
+		motor1Speed(0), motor2Speed(0),
+		motor1SpeedMax(motor1Max), motor2SpeedMax(motor2Max),
+		motor1SpeedMin(motor1Min), motor2SpeedMin(motor2Min)
+{
+	this->serialPort.setConfig(115200, 0, 8, 1, false);//Non configurable port setting see the roboteq manual
+	if(echoEnabled) {
+		this->enableSerialEcho(); //enable the serial to echo the commands back used for error checking
+	}else {
+		this->disableSerialEcho();//disable serial port echo
+	}
+};
 
 bool MotorController::setSpeed( MotorChannel channel, int value ) {
 	bool rval = false;
@@ -47,14 +63,52 @@ bool MotorController::setSpeed( MotorChannel channel, int value ) {
 	return rval;
 }
 
+bool MotorController::emergencyStop() {
+	return this->sendCommand("!EX\n\r");
+}
+
+bool MotorController::clearEmergencyStop() {
+	return this->sendCommand("!MG\n\r");
+}
+
+bool MotorController::reset() {
+	stringstream stream;
+	stream << "%RESET " << MotorController::MOTOR_SECRET_KEY << "\n\r";
+	return this->sendCommand(stream.str());
+}
+
+bool MotorController::setTime( int hours, int minutes, int seconds) {
+	stringstream stream;
+	stream << "%STIME " << hours << ":" << minutes << ":" << seconds << "\n\r";
+	return this->sendCommand(stream.str());
+}
+
 void MotorController::doProcess() {
-	if( this->serialPort.isOpen() ) {
 		stringstream parser;
 		parser << "!M " << this->motor1Speed << " " << this->motor2Speed << "\n\r";
-		this->serialPort.Write(parser.str().c_str(), parser.str().length());
-	}
+		this->sendCommand(parser.str());
 }
 
 bool MotorController::assertValidMotorRange( int max, int min, int value ) const {
 	return ( value > min && value < max );
+}
+
+bool MotorController::sendCommand( std::string command ) {
+	bool rval = false;
+	if( this->serialPort.isOpen() ) {
+		this->serialPort.Write(command.c_str(), command.length());
+	}
+	if( this->echoEnabled ) {
+		//check if the response is valid and return
+	}
+	rval = true;//TODO change this to detect verification of the command
+	return rval;
+}
+
+void MotorController::enableSerialEcho() {
+	this->sendCommand("^ECHOF 0\n\r");
+}
+
+void MotorController::disableSerialEcho() {
+	this->sendCommand("^ECHOF 1\n\r");
 }
