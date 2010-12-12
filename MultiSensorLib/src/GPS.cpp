f57fc73ab892ec3852e4cc5c612506525e100196
e2thenegpii <e2thenegpii@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2010-12-12 01:49:50 +0000

    Made the gps driver support both the Novatel and the PocketMAX gps. Eldon

--- MultiSensorLib/src/GPS.cpp
+++ MultiSensorLib/src/GPS.cpp
@@ -23,33 +23,43 @@ GPS::~GPS() {
 //This function ought to be called after loadConfig but before initialize
 void GPS::initConfig( mrpt::utils::CConfigFileBase & config, const std::string & sectionName) {
 
+	this->vendor = config.read_string(sectionName, "GPS_TYPE", "Novatel" );
 	this->isGpggaUsed = config.read_bool(sectionName,"use_gga", true);
 	this->isGprmcUsed = config.read_bool(sectionName,"use_rmc", false);
 	this->baudRate = config.read_int(sectionName,"baudRate", 57600);
 	this->portName = config.read_string(sectionName,"COM_port_LIN","/dev/ttyS0");
+	this->processRate = config.read_int(sectionName,"process_rate", 1);
 
 }
 
 void GPS::initialize() {
-	string writeValue;
 
 	cout << "here" << endl;
 	CSerialPort myCom(this->portName);
 	myCom.setConfig(this->baudRate,0,8,1,false);
 	cout << "Post open" << endl;
 
+	stringstream inputStream;
 	if( this->isGpggaUsed ) {
 		cout << "Using gpgga" << endl;
-		//writeValue = "$jasc,gpgga,1\n\r";  for other receiver
-		writeValue = "log gpgga ontime 1\n\r";
-		myCom.Write(writeValue.c_str(),writeValue.length());//this will tell the gps to get a satellite reading once a second
+		if( "PocketMAX" == this->vendor )
+			inputStream << "$jasc,gpgga," << this->processRate << "\n\r";
+		else if( "Novatel" == this->vendor )
+			inputStream << "log gpgga ontime " << this->processRate << "\n\r";
+		else
+			cerr << "Unsupported vendor" << endl;
+		myCom.Write(inputStream.str().c_str(),inputStream.str().length());//this will tell the gps to get a satellite reading once a second
 	}
 
 	if( this->isGprmcUsed ) {
 		cout << "Using gprmc" << endl;
-		//writeValue = "$jasc,gprmc,1\n\r";
-		writeValue = "log gprmc ontime 1\n\r";  // For Novatel's receiver
-		myCom.Write(writeValue.c_str(),writeValue.length());//this will tell the gps to run the nmea rmc command once a second
+		if( "PocketMAX")
+			inputStream << "$jasc,gprmc," << this->processRate << "\n\r";
+		else if( "Novatel" == this->vendor )
+			inputStream << "log gprmc ontime " << this->processRate << "\n\r";
+		else
+			cerr << "Unsupported vendor" << endl;
+		myCom.Write(inputStream.str().c_str(),inputStream.str().length());//this will tell the gps to run the nmea rmc command once a second
 	}
 
 	myCom.close();
