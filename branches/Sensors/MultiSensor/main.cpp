76478279712d9b948e7e33fd0856d45dc3dcdb07
e2thenegpii <e2thenegpii@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2010-12-28 01:51:23 +0000

    added the gps and the compass to this project as well as some basic path planning.  Eldon

--- branches/Sensors/MultiSensor/main.cpp
+++ branches/Sensors/MultiSensor/main.cpp
@@ -9,27 +9,86 @@
 #include <vector>
 
 #include <mrpt/poses/CPoint2D.h>
+#include <mrpt/utils/CConfigFile.h>
 
-#include "TSPSolver.h"
+#include "Compass.h"
+#include "GPS.h"
+#include "WaypointPlanner.h"
 
 using namespace std;
 using namespace mrpt;
+using namespace mrpt::slam;
 using namespace mrpt::poses;
+using namespace mrpt::utils;
+using namespace mrpt::system;
+using namespace mrpt::hwdrivers;
 
-/**
- * A test driver for the TSPSolver which is a class that solves the traveling sales man problem
- */
+void initCompass( Compass & compass );
+void initGPS( GPS & gps );
+
 int main( int argc, char** argv ) {
 
-	TSPSolver solver(40.247605, -111.648192);
+	Compass compass(false);
+	initCompass(compass);
 
-	solver.loadPoints("gpsWeighpoints_raw.txt");
+	GPS gps;
+	initGPS(gps);
 
-	vector<CPoint2D> visited = solver.solve();
+	cout << gps.isGPS_connected() << endl;
+
+	AbstractNavigationInterface * solver = new SequentialNavigation(40.247605, -111.648192);
+	solver->loadPoints("gpsWeighpoints_raw.txt");
+	vector<CPoint2D> visited = solver->solve();
+
+	while(!mrpt::system::os::kbhit()) {
+		mrpt::system::sleep( 500 );
+		compass.doProcess();
+		gps.doProcess();
+
+		CGenericSensor::TListObservations				lstObs;
+		CGenericSensor::TListObservations::iterator 	itObs;
+
+		gps.getObservations( lstObs );
+
+		if( compass.isYawValid() ) cout << "Yaw: " << compass.getYaw() << endl;
+		if( compass.isPitchValid() ) cout << "Pitch: " << compass.getPitch() << endl;
+		if( compass.isRollValid() ) cout << "Roll: " << compass.getRoll() << endl;
+		cout << endl;
+
+		for (itObs=lstObs.begin();itObs!=lstObs.end();itObs++)
+		{
+			ASSERT_(itObs->second->GetRuntimeClass()==CLASS_ID(CObservationGPS));
+
+			CObservationGPSPtr gpsData=CObservationGPSPtr(itObs->second);
+
+			gpsData->dumpToConsole();
+
+		}
+
+		lstObs.clear();
+
+	}
 
 	cout.precision(10);
 	for(vector<CPoint2D>::iterator point = visited.begin(); point != visited.end(); ++point )
-		cout << point->m_coords[TSPSolver::LAT] << " " << point->m_coords[TSPSolver::LON] << endl;
+		cout << point->m_coords[TSPNavigation::LAT] << " " << point->m_coords[TSPNavigation::LON] << endl;
 
+	delete solver;
 	return 0;
+}
+
+void initCompass( Compass & compass ) {
+	CConfigFile config( "MS.ini" );
+
+	compass.loadConfig(config, "COMPASS" );
+	compass.initialize();
+}
+
+void initGPS( GPS & gps ) {
+	gps.setSerialPortName ( "ttyUSB0" );
+	CConfigFile config("MS.ini");
+	gps.loadConfig(config, "GPS");
+	gps.initConfig(config, "GPS");
+
+	gps.initialize();
 }
