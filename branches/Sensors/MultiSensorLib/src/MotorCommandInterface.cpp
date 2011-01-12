a479218e7dcb3e234524d5d5305d78cd7805e8a7
e2thenegpii <e2thenegpii@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-01-12 19:54:57 +0000

    Eldon: Added Base class for the motor command interface.

---
+++ branches/Sensors/MultiSensorLib/src/MotorCommandInterface.cpp
@@ -0,0 +1,17 @@
+/*
+ * MotorCommandInterface.cpp
+ *
+ *  Created on: Jan 12, 2011
+ *      Author: tallred3
+ */
+
+#include "MotorCommandInterface.h"
+
+MotorCommandInterface::MotorCommandInterface() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MotorCommandInterface::~MotorCommandInterface() {
+	// TODO Auto-generated destructor stub
+}
