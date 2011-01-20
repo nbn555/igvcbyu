3ad4d594d15dc4f2c134f5012453207a611189ff
e2thenegpii <e2thenegpii@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-01-20 07:14:21 +0000

    Fixed MotorCommand class.  Eldon

---
+++ MultiSensorLib/Debug/src/MotorCommand.d
@@ -0,0 +1,106 @@
+src/MotorCommand.d src/MotorCommand.o: ../src/MotorCommand.cpp \
+ /home/tallred3/ecen493/YClopsLib/inc/MotorCommand.h \
+ /home/tallred3/ecen493/YClopsLib/inc/MotorCommandInterface.h \
+ /home/tallred3/ecen493/YClopsLib/inc/MotorController.h \
+ /usr/include/mrpt/hwdrivers/include/mrpt/hwdrivers/CSerialPort.h \
+ /usr/include/mrpt/mrpt-config/mrpt/config.h \
+ /usr/include/mrpt/base/include/mrpt/utils/CStream.h \
+ /usr/include/mrpt/base/include/mrpt/utils/utils_defs.h \
+ /usr/include/mrpt/base/include/mrpt/system/os.h \
+ /usr/include/mrpt/base/include/mrpt/base/link_pragmas.h \
+ /usr/include/mrpt/base/include/mrpt/utils/boost_join.h \
+ /usr/include/mrpt/base/include/mrpt/utils/types.h \
+ /usr/include/mrpt/base/include/mrpt/utils/pstdint.h \
+ /usr/include/mrpt/base/include/mrpt/math/math_frwds.h \
+ /usr/include/mrpt/base/include/mrpt/utils/compiler_fixes.h \
+ /usr/include/mrpt/base/include/mrpt/utils/mrpt_macros.h \
+ /usr/include/mrpt/base/include/mrpt/otherlibs/stlplus/smart_ptr.hpp \
+ /usr/include/mrpt/base/include/mrpt/otherlibs/stlplus/containers_fixes.hpp \
+ /usr/include/mrpt/base/include/mrpt/otherlibs/stlplus/exceptions.hpp \
+ /usr/include/mrpt/base/include/mrpt/synch/atomic_incr.h \
+ /usr/include/mrpt/base/include/mrpt/otherlibs/stlplus/smart_ptr.tpp \
+ /usr/include/mrpt/base/include/mrpt/utils/bits.h \
+ /usr/include/mrpt/base/include/mrpt/utils/CUncopiable.h \
+ /usr/include/mrpt/base/include/mrpt/utils/CObject.h \
+ /usr/include/mrpt/base/include/mrpt/utils/safe_pointers.h \
+ /usr/include/mrpt/base/include/mrpt/utils/exceptions.h \
+ /usr/include/mrpt/base/include/mrpt/utils/CTicTac.h \
+ /usr/include/mrpt/base/include/mrpt/utils/stl_extensions.h \
+ /usr/include/mrpt/base/include/mrpt/utils/CSerializable.h \
+ /usr/include/mrpt/base/include/mrpt/utils/metaprogramming.h \
+ /usr/include/mrpt/base/include/mrpt/utils/circular_buffer.h \
+ /usr/include/mrpt/base/include/mrpt/utils/list_searchable.h \
+ /usr/include/mrpt/base/include/mrpt/utils/bimap.h \
+ /usr/include/mrpt/base/include/mrpt/utils/map_as_vector.h \
+ /usr/include/mrpt/base/include/mrpt/utils/traits_map.h \
+ /usr/include/mrpt/hwdrivers/include/mrpt/hwdrivers/link_pragmas.h
+
+/home/tallred3/ecen493/YClopsLib/inc/MotorCommand.h:
+
+/home/tallred3/ecen493/YClopsLib/inc/MotorCommandInterface.h:
+
+/home/tallred3/ecen493/YClopsLib/inc/MotorController.h:
+
+/usr/include/mrpt/hwdrivers/include/mrpt/hwdrivers/CSerialPort.h:
+
+/usr/include/mrpt/mrpt-config/mrpt/config.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/CStream.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/utils_defs.h:
+
+/usr/include/mrpt/base/include/mrpt/system/os.h:
+
+/usr/include/mrpt/base/include/mrpt/base/link_pragmas.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/boost_join.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/types.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/pstdint.h:
+
+/usr/include/mrpt/base/include/mrpt/math/math_frwds.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/compiler_fixes.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/mrpt_macros.h:
+
+/usr/include/mrpt/base/include/mrpt/otherlibs/stlplus/smart_ptr.hpp:
+
+/usr/include/mrpt/base/include/mrpt/otherlibs/stlplus/containers_fixes.hpp:
+
+/usr/include/mrpt/base/include/mrpt/otherlibs/stlplus/exceptions.hpp:
+
+/usr/include/mrpt/base/include/mrpt/synch/atomic_incr.h:
+
+/usr/include/mrpt/base/include/mrpt/otherlibs/stlplus/smart_ptr.tpp:
+
+/usr/include/mrpt/base/include/mrpt/utils/bits.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/CUncopiable.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/CObject.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/safe_pointers.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/exceptions.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/CTicTac.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/stl_extensions.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/CSerializable.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/metaprogramming.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/circular_buffer.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/list_searchable.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/bimap.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/map_as_vector.h:
+
+/usr/include/mrpt/base/include/mrpt/utils/traits_map.h:
+
+/usr/include/mrpt/hwdrivers/include/mrpt/hwdrivers/link_pragmas.h:
---
+++ MultiSensorLib/src/MotorCommand.cpp
@@ -0,0 +1,27 @@
+/*
+ * MotorCommands.cpp
+ *
+ *  Created on: Jan 11, 2011
+ *      Author: igvcbyu
+ */
+
+#include "MotorCommand.h"
+
+MotorCommand::MotorCommand(MotorController * mC, double lC, double aC): MotorCommandInterface(mC), linearConst(lC), angConst(aC) {
+}
+
+void MotorCommand::doProcess() {
+	this->Go(this->linearVelocity,this->angularVelocity);
+}
+
+bool MotorCommand::Go(float linear, float angular){
+	int rmp1= linearConst*linear + angConst*angular;
+	int rmp2= linearConst*linear - angConst*angular;
+	if(!(this->motorPtr->setSpeed(MotorController::Channel1, rmp1)&&this->motorPtr->setSpeed(MotorController::Channel2, rmp2)))
+		return false;
+	return true;
+}
+
+MotorCommand::~MotorCommand() {
+	// TODO Auto-generated destructor stub
+}
