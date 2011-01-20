a587c8b0d51afa8581e3bee510bdc43513c332ce
e2thenegpii <e2thenegpii@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-01-20 06:03:17 +0000

    Initial import of wii driver.  Instructions are in comments in main.  Eldon

---
+++ WiiDriver/main.cpp
@@ -0,0 +1,66 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 21, 2010
+ *      Author: igvcbyu
+ */
+
+#include <iostream>
+#include <stdlib.h>
+
+#include <mrpt/utils/CConfigFile.h>
+
+#include "MotorController.h"
+#include "JoystickCommand.h"
+
+#include <mrpt/hwdrivers/CSerialPort.h>
+
+using namespace mrpt::utils;
+using namespace std;
+
+/*
+ * Quick How to
+ * make sure the /dev/uinput device exsists type
+ * ls -lah /dev/uinput
+ * if it doesn't exsist type
+ * sudo modprobe uinput
+ *
+ * For ubuntu
+ * this will only last until the next reboot
+ * or add the /etc/modules file by adding the following
+ * su -c 'echo "uinput">>/etc/modules'
+ * reboot or run the modprobe command above
+ * end ubuntu
+ *
+ * Make sure permissions for /dev/uinput are 666 if not run
+ * sudo chmod 666 /dev/uinput
+ * check the default config file is the gamepad file by typing
+ * ls -lah /etc/cwiid/wminput
+ * you should see something like "default -> gamepad"
+ * if not delete the default link and create a new one with the following
+ * sudo rm default
+ * sudo ln -s gamepad default
+ *
+ * make sure the config file has the following
+ * then run the wminput command
+ * wminput
+ * then press buttons 1 and 2 on the wiimote
+ * once the bluetooth link has been established run where the astrisk is the index of the joystick (should be zero)
+ * jscal -c /dev/input/js*
+ * follow the prompts
+ * run the wiiDriver program
+ */
+int main( int argc, char** argv ) {
+
+	MotorController motor("/dev/ttyUSB0",true);
+	JoystickCommand js(&motor);
+
+	while(1) {
+
+		js.doProcess();
+		motor.doProcess();
+
+	}
+
+	return 0;
+}
