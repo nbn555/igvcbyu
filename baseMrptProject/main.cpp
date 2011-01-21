9297da0257c7f484fcf6a443b91a9f68c9dc96ff
e2thenegpii <e2thenegpii@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-01-21 18:29:10 +0000

    Initital include this is a project to copy the mrpt project settings you can make a copy in eclipse and then not have to worry about the include paths etc as long as you have YClops project set up or a linkable libYClops.a Eldon

---
+++ baseMrptProject/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 21, 2010
+ *      Author: igvcbyu
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main( int argc, char** argv ) {
+	cout << "Hello World!" << endl;
+	return 0;
+}
