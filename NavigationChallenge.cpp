518f0b162fff492176d902920e90407c6e62d0e2
igvcbyu <igvcbyu@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-01-28 20:36:48 +0000

    added comments for clarification and to help with testing tomorrow if anything goes wrong

--- NavigationChallenge.cpp
+++ NavigationChallenge.cpp
@@ -15,21 +15,26 @@
 void NavigationChallenge::AutonomousMode(string& waypointsFile, bool challange)
 {
 	string motorControllerPort = "ttyS000";
+	//interface that will be used by the reactive nav to sense the enviroment and make the robot move
 	YclopsReactiveNavInterface* interface = new YclopsReactiveNavInterface(motorControllerPort);
-
+	//intial position
 	mrpt::poses::CPose2D pose = CPoint2D();
 	float v = 0;
 	float w = 0;
+
 
 	interface->getCurrentPoseAndSpeeds(pose, v,w);
 	double lat = pose.x();
 	double lon = pose.y();
 
+	// solves the tsp problem, it autonomous challenge we need to add some code here to make it work.
 	TSPNavigation nav = TSPNavigation(lat,lon);
 
 	nav.loadPoints(waypointsFile);
+	//waypoints in the order we want to visit them
 	mrpt::aligned_containers<mrpt::poses::CPoint2D>::vector_t points = nav.solve();
 
+	//the reactive nav to get from the current position to the next point the tsp solver wants us to go to
 	mrpt::reactivenav::YclopsNavigationSystem reactivenav = mrpt::reactivenav::YclopsNavigationSystem(*interface, false, false);
 
 	mrpt::utils::CConfigFile navConfig("robot.ini");
@@ -40,17 +45,20 @@ void NavigationChallenge::AutonomousMode(string& waypointsFile, bool challange)
 	mrpt::aligned_containers<mrpt::poses::CPoint2D>::vector_t::iterator iter = points.begin();
 	mrpt::aligned_containers<mrpt::poses::CPoint2D>::vector_t::iterator end = points.end();
 
-
+	//go until there aren't anymore waypoints
 	while (iter != end)
 	{
+		//setting up the next waypoint
 		mrpt::reactivenav::CAbstractReactiveNavigationSystem::TNavigationParams   navParams;
 		navParams.target.x = iter->x();
 		navParams.target.y = iter->y();
-		navParams.targetAllowedDistance = challange ?  0.40f: 20.0f;
+		//distance from the waypoint in meters before the robot considers itself at the point
+		navParams.targetAllowedDistance = challange ?  0.20f: 7.0f;
 		navParams.targetIsRelative = !challange;
 
+		//gives the reactive nav the next goal
 		reactivenav.navigate( &navParams );
-
+		//go until it makes it to the point
 		while(reactivenav.getCurrentState() ==
 				mrpt::reactivenav::CAbstractReactiveNavigationSystem::NAVIGATING)
 		{
@@ -58,5 +66,5 @@ void NavigationChallenge::AutonomousMode(string& waypointsFile, bool challange)
 		}
 		++iter;
 	}
-
+	//time for a vitory dance
 }
--- main.cpp
+++ main.cpp
@@ -11,8 +11,10 @@
 using namespace std;
 
 int main( int argc, char** argv ) {
+	//this makes a navigation challenge go, by loading the ini files and using points.txt + startPoint as waypoints.
 	NavigationChallenge nav = NavigationChallenge();
-	string file = "points.ini";
+	string file = "points.txt";
+	//true means it is a navigation challenge, needs some work for the false to work
 	nav.AutonomousMode(file, true);
 	return 0;
 }
