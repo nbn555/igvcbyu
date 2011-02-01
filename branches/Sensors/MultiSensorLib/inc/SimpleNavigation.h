cdbf2fc5fa6b5bee3ef6a4b610a9a61705cb9ad7
jazer2187@gmail.com <jazer2187@gmail.com@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-02-01 20:45:14 +0000

    adding simple navigation class

---
+++ branches/Sensors/MultiSensorLib/inc/SimpleNavigation.h
@@ -0,0 +1,17 @@
+/*
+ * SimpleNavigation.h
+ *
+ *  Created on: Feb 1, 2011
+ *      Author: quincyj2
+ */
+
+#ifndef SIMPLENAVIGATION_H_
+#define SIMPLENAVIGATION_H_
+
+class SimpleNavigation {
+public:
+	SimpleNavigation();
+	virtual ~SimpleNavigation();
+};
+
+#endif /* SIMPLENAVIGATION_H_ */
---
+++ branches/Sensors/MultiSensorLib/src/SimpleNavigation.cpp
@@ -0,0 +1,17 @@
+/*
+ * SimpleNavigation.cpp
+ *
+ *  Created on: Feb 1, 2011
+ *      Author: quincyj2
+ */
+
+#include "SimpleNavigation.h"
+
+SimpleNavigation::SimpleNavigation() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SimpleNavigation::~SimpleNavigation() {
+	// TODO Auto-generated destructor stub
+}
