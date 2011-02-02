5e698171cf4fec9d0b760c0882bc7ab5b7bf3352
e2thenegpii <e2thenegpii@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-02-02 08:09:00 +0000

    added support for the buttons and call back functions.  This will let us do helpful stuff with the buttons.  Eldon

--- branches/Sensors/MultiSensorLib/inc/JoystickCommand.h
+++ branches/Sensors/MultiSensorLib/inc/JoystickCommand.h
@@ -14,14 +14,18 @@
 
 class JoystickCommand: public MotorCommandInterface {
 public:
-	JoystickCommand( MotorController * mP );
+	JoystickCommand( MotorController * mP, int joystickIndex = 0 );
 	virtual ~JoystickCommand();
 
 	void doProcess();
+	void registerButton( unsigned int buttonIndex, void (*funct)(void*), void* data );
 private:
 	float x, y, z;
 	std::vector<bool> buttons;
+	std::vector<void(*)(void*)> functPtrs;
+	std::vector<void*> functPtrsData;
 	mrpt::hwdrivers::CJoystick joystick;
+	int joystickIndex;
 };
 
 #endif /* JOYSTICKCOMMAND_H_ */
--- branches/Sensors/MultiSensorLib/src/JoystickCommand.cpp
+++ branches/Sensors/MultiSensorLib/src/JoystickCommand.cpp
@@ -7,14 +7,34 @@
 
 #include "JoystickCommand.h"
 
-JoystickCommand::JoystickCommand( MotorController * mP ): MotorCommandInterface( mP ) {
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+//#define DEBUG
+
+JoystickCommand::JoystickCommand( MotorController * mP, int joystickIndex ): MotorCommandInterface( mP ), x(-1), y(-1), z(-1), joystickIndex(joystickIndex) {
+
+	//spin waiting for the joystick to get initialized
+	do {
+		this->joystick.getJoystickPosition(this->joystickIndex, this->x, this->y, this->z, this->buttons );
+#ifdef DEBUG
+		cout << "Waiting for joystick connection" << "x" << this->x << " y" << this->y << " z" << this->z << endl;
+#endif
+	} while( (this->x == -1) || (this->y == -1) || (this->z == -1) );
+
+	//once the joystick is initialized size the function pointers and their data pointers
+	cout << this->buttons.size() << " " << endl;
+	this->functPtrs = vector<void(*)(void*)>(this->buttons.size(), NULL);
+	this->functPtrsData = vector<void*>(this->buttons.size(), NULL);
 
 }
 
 JoystickCommand::~JoystickCommand() { }
 
 void JoystickCommand::doProcess() {
-		this->joystick.getJoystickPosition(0, this->x, this->y, this->z, this->buttons );
+		this->joystick.getJoystickPosition(this->joystickIndex, this->x, this->y, this->z, this->buttons );
 
 		int m1 = this->x * 1000;
 		int m2 = this->z * 1000;
@@ -22,4 +42,27 @@ void JoystickCommand::doProcess() {
 		this->motorPtr->setSpeed(MotorController::Channel1, m1);
 		this->motorPtr->setSpeed(MotorController::Channel2, m2);
 
+		for( unsigned int i = 0; i < this->buttons.size(); i++ ) {
+			if(this->buttons[i]) {
+				if( NULL != this->functPtrs[i])
+					this->functPtrs[i](this->functPtrsData[i]);
+			}
+		}
+
+#ifdef DEBUG
+		cout << x << " " << y << " " << z << " ";
+		for(unsigned int i = 0; i < this->buttons.size(); i++ )
+			cout << this->buttons[i];
+		cout << endl;
+#endif
+
 }
+
+void JoystickCommand::registerButton( unsigned int buttonIndex, void (*funct)(void*), void* data ) {
+
+	if(buttonIndex >= 0 && buttonIndex < this->functPtrs.size() ) {
+		this->functPtrs[buttonIndex] = funct;
+		this->functPtrsData[buttonIndex] = data;
+	}
+}
+
