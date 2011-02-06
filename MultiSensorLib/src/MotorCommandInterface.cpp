0b2df8478d415574a4853f6244a4ff2c68fdab0d
e2thenegpii <e2thenegpii@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-02-06 00:42:42 +0000

    Added the DualMotorController to replace the joystick object.  Eldon

--- MultiSensorLib/src/MotorCommandInterface.cpp
+++ MultiSensorLib/src/MotorCommandInterface.cpp
@@ -6,13 +6,14 @@
  */
 
 #include "MotorCommandInterface.h"
+#include "WiiController.h"
 
-MotorCommandInterface::MotorCommandInterface() {
+#include <iostream>
+using namespace std;
 
-}
+MotorCommandInterface::MotorCommandInterface() { }
 
-MotorCommandInterface::~MotorCommandInterface() {
-}
+MotorCommandInterface::~MotorCommandInterface() { }
 
 bool MotorCommandInterface::setVelocity( double linearVelocity, double angularVelocity ) {
 	this->linearVelocity = linearVelocity;
@@ -22,6 +23,29 @@ bool MotorCommandInterface::setVelocity( double linearVelocity, double angularVe
 
 bool MotorCommandInterface::getSuccess() {
 	return this->success;
+}
+
+DualMotorCommand::DualMotorCommand() { }
+
+DualMotorCommand::~DualMotorCommand() { }
+
+void DualMotorCommand::doProcess() {
+	WiiController * controller = WiiController::getReference();
+	uint16_t lxaxis, lyaxis;
+	uint16_t rxaxis, ryaxis;
+	controller->getLeftStick( lxaxis, lyaxis );
+	controller->getRightStick( rxaxis, ryaxis );
+
+	double lspeed = ((lyaxis-32)/32.0)*1000; //need to divide by 32 because the l stick gives us 6 bits precision
+	double rspeed = ((ryaxis-16)/16.0)*1000; //need to divide by 16 because the r stick gives us 5 bits precision
+
+	cout << "lyaxis:" << lyaxis << "Lspeed " << (int)lspeed << endl;
+	cout << "ryaxis:" << ryaxis << "Rspeed " << (int)rspeed << endl;
+
+//		MotorController::instance()->setSpeed(MotorController::Channel1, (int)lspeed);
+//		MotorController::instance()->setSpeed(MotorController::Channel2, (int)rspeed);
+//		MotorController::instance()->doProcess();
+
 }
 
 DummyMotorCommand::DummyMotorCommand() {
