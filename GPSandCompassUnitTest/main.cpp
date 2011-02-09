00599c258307cb3551a64d1411fdd3ffda016c46
igvc.byu <igvc.byu@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-02-09 18:12:40 +0000

    created GPS and Compass Unit Test projects

---
+++ GPSandCompassUnitTest/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 21, 2010
+ *      Author: igvcbyu
+ */
+
+#include <iostream>
+#include <mrpt/hwdrivers/CGPSInterface.h>
+
+using namespace mrpt;
+using namespace mrpt::utils;
+using namespace mrpt::slam;
+using namespace mrpt::system;
+using namespace mrpt::hwdrivers;
+using namespace std;
+
+int main( int argc, char** argv ) {
+	cout << "Hello World!" << endl;
+	return 0;
+}
