2d2b8debb54b6fe85d956bb84dc288548ed96943
igvc.byu@gmail.com <igvc.byu@gmail.com@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-02-09 06:51:22 +0000

    Added C code to beep the internal speaker.  However we need root access for it to work.  Eldon

--- branches/Sensors/MultiSensorLib/src/Beeper.cpp
+++ branches/Sensors/MultiSensorLib/src/Beeper.cpp
@@ -10,10 +10,14 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <linux/kd.h>
 
 using namespace std;
-
-Beeper::beep_t Beeper::buffer;
 
 Beeper::Beeper() {
 }
@@ -23,20 +27,19 @@ Beeper::~Beeper() {
 
 void Beeper::beep( int frequency, int length ) {
 	pthread_t t;
-	buffer = beep_t(frequency,length);
-	pthread_create( &t, NULL, Beeper::beepi, &Beeper::buffer );
+	beep_t buffer = beep_t(frequency,length);//This may be a problem taking the address of a local
+	pthread_create( &t, NULL, Beeper::beepi, &buffer );
 	pthread_detach( t );
 
 }
 
 void * Beeper::beepi( void * data ) {
 	fprintf(stdout, "beep\n");
-	system("beep");
-	//int fd = open( "/dev/tty0", O_RDONLY );
+	//system("beep");
+	int fd = open( "/dev/console", O_RDONLY );
 	beep_t * thisBeep = ((beep_t*)data);
-	//int arg = (time<<16)+(1193180/(thisBeep->frequency));
-	//ioctl(fd, KDMKTONE,arg);
-	//sleep(1);
+	int arg = ((thisBeep->length)<<16)+(1193180/(thisBeep->frequency));
+	ioctl(fd, KDMKTONE,arg);
 	fprintf(stdout, "beep done\n");
 	pthread_exit(NULL);
 	return NULL;
