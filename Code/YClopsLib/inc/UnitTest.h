3ddcab72592bd30c2ab9b69aa656877642daf412
e2thenegpii <e2thenegpii@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-02-23 06:01:16 +0000

    added unit testing and changed to shared library.  Eldon

---
+++ Code/YClopsLib/inc/UnitTest.h
@@ -0,0 +1,19 @@
+/*
+ * UnitTest.h
+ *
+ *  Created on: Feb 22, 2011
+ *      Author: tallred3
+ */
+
+#ifndef UNITTEST_H_
+#define UNITTEST_H_
+
+class UnitTest {
+public:
+	static bool testCompass();
+private:
+	UnitTest(){};
+	virtual ~UnitTest(){};
+};
+
+#endif /* UNITTEST_H_ */
