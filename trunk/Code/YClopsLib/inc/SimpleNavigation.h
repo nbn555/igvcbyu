e85a7bf41190b8f8a7593cde01864565ca9dc840
igvcbyu <igvcbyu@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-02-24 20:00:54 +0000

    Added the following methods: method to accept waypoints filename and a boolean showing if the waypoints are in meters or longitude method to calculate distance based on meters method to calculate bearing based on meters
    To Do:
    Shift bearing according to north, ie from Graph style -> Compass Style
    Check meters flag and change things accordingly.
    implement logging

--- trunk/Code/YClopsLib/inc/SimpleNavigation.h
+++ trunk/Code/YClopsLib/inc/SimpleNavigation.h
@@ -15,15 +15,44 @@
 
 class SimpleNavigation {
 private:
-	string& fileName;
+	string fileName;
+	bool inMeters;
 	mrpt::reactivenav::CReactiveInterfaceImplementation* interface;
+	mrpt::aligned_containers<mrpt::poses::CPoint2D>::vector_t points;
+	mrpt::reactivenav::CAbstractReactiveNavigationSystem::TState state;
+	mrpt::reactivenav::CAbstractReactiveNavigationSystem::TNavigationParams* navParams;
+	void gotoNextPoint();
+	float distance(mrpt::poses::CPose2D& curPose)
+	{
+		float x1 = curPose.x();
+		float y1 = curPose.y();
+		float x2 = navParams->target.x;
+		float y2 = navParams->target.y;
+		double dist = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+		cout << x1 << " " << y1 << " is a distance of " << dist << " from " << x2 << " " << y2 << endl;
+		return dist;
+	}
+
+	double calcBearing(mrpt::poses::CPose2D& curPose)
+	{
+		float x1 = curPose.x();
+		float y1 = curPose.y();
+		float x2 = navParams->target.x;
+		float y2 = navParams->target.y;
+		float dx = x2 - x1;
+		float dy = y2 - y1;
+		return atan2(dy,dx);
+	}
 public:
 
 	SimpleNavigation(string & fileName, mrpt::reactivenav::CReactiveInterfaceImplementation* interface);
 	virtual ~SimpleNavigation();
-	void setFileName(string & fileName);
+	void setFileName(string & fileName, bool inMeters);
 	void navigate( mrpt::reactivenav::CAbstractReactiveNavigationSystem::TNavigationParams *navParams );
 	void go();
+	void navigationStep();
+	void loadConfigFile(mrpt::utils::CConfigFileBase& interfaceConfig, mrpt::utils::CConfigFileBase& robotConfig){ ; }
+	mrpt::reactivenav::CAbstractReactiveNavigationSystem::TState getCurrentState(){ return state;}
 };
 
 #endif /* SIMPLENAVIGATION_H_ */
