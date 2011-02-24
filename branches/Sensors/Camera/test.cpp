72705ad9044c5864f5b8cfe7d8d3997ae34e03f5
igvc.byu@gmail.com <igvc.byu@gmail.com@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-02-24 19:06:05 +0000

    added more testing code

--- branches/Sensors/Camera/test.cpp
+++ branches/Sensors/Camera/test.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <cassert>
 
 #include "Camera.h"
 #include "logging.h"
@@ -16,10 +17,19 @@ using namespace cv;
 
 
 int main(int argc, char *argv[]) {
+	mrpt::utils::CConfigFile config("Camera.ini");
+	Log::SetReportLevel(DEBUG4);
+	Log::SetLogFile(&cout);
+
+
 	Camera camera;
-	//camera.loadConfig("test");
+
+	camera.loadConfig(config, "CAMERA");
 
 	camera.startCamera();
+
+	cout << "Done..." << endl;
 	//camera.getObstacles(NULL);
+
 }
 
