104d72a253218e13a49a4fdba2e2a4311917374b
e2thenegpii <e2thenegpii@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-03-26 07:58:50 +0000

    Added the HumanInputInterface class which abstracts the wii controller away from the program so that we can use a keyboard or other input device instead.  Eldon

--- WiiDriver/main.cpp
+++ WiiDriver/main.cpp
@@ -12,10 +12,10 @@
 #include "MotorController.h"
 #include "MotorCommandInterface.h"
 #include "Compass.h"
+#include "HumanInputInterface.h"
 #include "WiiController.h"
 #include "YClopsReactiveNavInterface.h"
 #include "YclopsNavigationSystem.h"
-#include "Beeper.h"
 #include "logging.h"
 #include "YClopsConfiguration.h"
 
@@ -24,19 +24,24 @@
 using namespace mrpt::utils;
 using namespace std;
 
-bool closing = false;
-
 void signal_handler( int signum );
 void shutdown( int exitStatus );
-LOG_LEVEL loggingLevel = DEBUG4;
+void handleUserInput();
 
 YClopsReactiveNavInterface * yclops = NULL;
 mrpt::reactivenav::YclopsNavigationSystem * ai = NULL;
 ofstream of;
 const string DEFAULT_POINTS_FILE = "points.txt";
 
+/*
+ * @TODO make a compass calibrator program
+ * @TODO make a configuration file gui
+ * @TODO make a human input hardware interface layer to remove the necessity of using the wii controller
+ * @TODO create an serial port command interface to refactor the serial port sending and response sending
+ */
 int main( int argc, char** argv ) {
 
+	bool badClose = false;
 	try{
 		//test if the config file is passed in
 		if(argc < 2) {
@@ -53,7 +58,7 @@ int main( int argc, char** argv ) {
 		//Set up logging
 		//Log::SetLogFile(&of);
 		Log::SetLogFile(&cout);
-		Log::SetReportLevel(loggingLevel);
+		Log::SetReportLevel(DEBUG4);
 		Log::SetTimeStampDisplay(false);
 		Log::SetReportStreamBits(ALL_LOG&(~(WII_LOG)));
 		//Log::SetReportStreamBits(ALL_LOG);
@@ -65,8 +70,8 @@ int main( int argc, char** argv ) {
 		signal(SIGINT, signal_handler);
 
 		//Initialize the WiiController
-		LOG(DEBUG4) << "Creating the new Wii Controller" << endl;
-		WiiController::create();
+		LOG(DEBUG4) << "Creating the User Input Device" << endl;
+		HumanInputInterface::createWiiController();
 
 		LOG(DEBUG4) << "Creating YClops Object" << endl;
 		yclops = new YClopsReactiveNavInterface();
@@ -78,8 +83,11 @@ int main( int argc, char** argv ) {
 		ai->loadConfigFile(YClopsConfiguration::instance(), YClopsConfiguration::instance());
 
 		LOG(DEBUG4) << "Starting main loop" << endl;
-		while(!closing) {
-			LOG(DEBUG4) << "Iterating: " << (!closing?"true":"false") << endl;
+		while(!HumanInputInterface::getClosingFlag()) {
+			LOG(DEBUG4) << "Iterating: " << (!HumanInputInterface::getClosingFlag()?"true":"false") << endl;
+
+			if(HumanInputInterface::instance()->getInputFlag())
+				HumanInputInterface::instance()->handleUserInput();
 
 			switch(ai->getCurrentState()) {
 			case mrpt::reactivenav::CAbstractReactiveNavigationSystem::IDLE:
@@ -104,10 +112,10 @@ int main( int argc, char** argv ) {
 		}
 	} catch (...) {
 		LOG(ERROR) << "Unhandled Exception caught" << endl;
-		closing = false; //We are closing but we close with an error see the shutdown call below
+		badClose = true; //We are closing with an error see the shutdown call below
 	}
 
-	shutdown((closing?EXIT_SUCCESS:EXIT_FAILURE));
+	shutdown((badClose?EXIT_FAILURE:EXIT_SUCCESS));
 
 	return EXIT_FAILURE;
 }
@@ -120,183 +128,17 @@ void signal_handler( int signum ) {
 			shutdown(EXIT_FAILURE);
 		}
 	} else if( SIGUSR1 == signum ) {
-		uint16_t cbuttons = 0;
-		uint16_t lax = 0, lay = 0;
-		uint16_t rax = 0, ray = 0;
-		uint16_t la = 0;
-		uint16_t ra = 0;
-		uint16_t mbuttons = 0;
-
-		WiiController * controller = WiiController::getReference();
-
-		controller->getClassicButtons(cbuttons);
-		controller->getLeftStick(lax,lay);
-		controller->getRightStick(rax,ray);
-		controller->getLeftAnalog(la);
-		controller->getRightAnalog(ra);
-		controller->getMoteButtons(mbuttons);
-
-		if( cbuttons & CLASSIC_D_UP ) {
-			LOG(INFO) << "Toggling camera data" << endl;
-			yclops->toggleCameraDump();
-		}
-
-		if( cbuttons & CLASSIC_D_DOWN ) {
-			LOG(INFO) << "Toggling lidar data" << endl;
-			yclops->toggleLidarDump();
-		}
-
-		if( cbuttons & CLASSIC_D_LEFT ) {
-			LOG(INFO) << "Toggling GPS data" << endl;
-			yclops->toggleGpsDump();
-		}
-
-		if( cbuttons & CLASSIC_D_RIGHT ) {
-			LOG(INFO) << "Toggling Compass data" << endl;
-			yclops->toggleCompassDump();
-		}
-
-		if( cbuttons & CLASSIC_A ) {
-			LOG(INFO) << "Going into Autonomous Mode" << endl;
-			yclops->setAutonomousMode();
-			ai->setChallenge(false);
-			string pointsFile = YClopsConfiguration::instance().read_string("GLOBAL_CONFIG","POINTS_FILE","points.txt");
-			ai->setFileName(pointsFile,false);
-			ai->setup();
-		}
-
-		if( cbuttons & CLASSIC_B ) {
-			LOG(INFO) << "Going into Navigation Mode" << endl;
-			yclops->setNavigationMode();
-			ai->setChallenge(true);
-			string pointsFile = YClopsConfiguration::instance().read_string("GLOBAL_CONFIG","POINTS_FILE","points.txt");
-			ai->setFileName(pointsFile,false);
-			ai->setup();
-		}
-
-		if( cbuttons & CLASSIC_X ) {
-			LOG(INFO) << "Idling" << endl;
-			yclops->useNullMotorCommand();
-			yclops->setIdle();
-			ai->stop();
-		}
-
-		if( cbuttons & CLASSIC_Y ) {
-			LOG(INFO) << "Wii Motor Control" << endl;
-			yclops->useWiiMotorCommand();
-			ai->stop();
-		}
-
-		if( cbuttons & CLASSIC_L1 ) {
-			LOG_WII(DEBUG4) << "Classic L1" << endl;
-		}
-
-		if( cbuttons & CLASSIC_L2 ) {
-			LOG(INFO) << "Toggling Encoder data" << endl;
-			yclops->toggleEncoderDump();
-		}
-
-		if( cbuttons & CLASSIC_R1 ) {
-			LOG_WII(DEBUG4) << "Classic R1" << endl;
-		}
-
-		if( cbuttons & CLASSIC_R2 ) {
-			LOG_WII(DEBUG4) << "Classic R2" << endl;
-		}
-
-		if( cbuttons & CLASSIC_SELECT ) {
-			switch(loggingLevel) {
-			case DISABLE:							break;
-			case FATAL:		loggingLevel = ERROR; 	break;
-			case ERROR:		loggingLevel = WARNING;	break;
-			case WARNING:	loggingLevel = INFO;	break;
-			case INFO:		loggingLevel = DEBUG;	break;
-			case DEBUG:		loggingLevel = DEBUG2;	break;
-			case DEBUG2:	loggingLevel = DEBUG3;	break;
-			case DEBUG3:	loggingLevel = DEBUG4;	break;
-			case DEBUG4:							break;
-			default:								break;
-			}
-			(*Log::GetOStream()) << "Log level: " << Log::ToString( loggingLevel ) << endl;
-			Log::SetReportLevel(loggingLevel);
-		}
-
-		if( cbuttons & CLASSIC_HOME ) {
-			LOG(DEBUG4) << "Shutdown button pressed" << endl;
-			closing = true;
-		}
-
-		if( cbuttons & CLASSIC_START ) {
-			switch(loggingLevel) {
-			case DISABLE:							break;
-			case FATAL: 						 	break;
-			case ERROR:		loggingLevel = FATAL;	break;
-			case WARNING:	loggingLevel = ERROR;	break;
-			case INFO:		loggingLevel = WARNING;	break;
-			case DEBUG:		loggingLevel = INFO;	break;
-			case DEBUG2:	loggingLevel = DEBUG;	break;
-			case DEBUG3:	loggingLevel = DEBUG2;	break;
-			case DEBUG4:	loggingLevel = DEBUG3;	break;
-			default:								break;
-			}
-			(*Log::GetOStream()) << "Log level: " << Log::ToString( loggingLevel ) << endl;
-			Log::SetReportLevel(loggingLevel);
-
-		}
-
-		if( mbuttons & MOTE_D_UP ) {
-			LOG_WII(DEBUG4) << "Mote D Up" << endl;
-		}
-
-		if( mbuttons & MOTE_D_LEFT ) {
-			LOG_WII(DEBUG4) << "Mote D Left" << endl;
-		}
-
-		if( mbuttons & MOTE_D_RIGHT ) {
-			LOG_WII(DEBUG4) << "Mote D Right" << endl;
-		}
-
-		if( mbuttons & MOTE_D_DOWN ) {
-			LOG_WII(DEBUG4) << "Mote D Down" << endl;
-		}
-
-		if( mbuttons & MOTE_1 ) {
-			LOG_WII(DEBUG4) << "Mote 1" << endl;
-		}
-
-		if( mbuttons & MOTE_2 ) {
-			LOG_WII(DEBUG4) << "Mote 2" << endl;
-		}
-
-		if( mbuttons & MOTE_PLUS ) {
-			LOG_WII(DEBUG4) << "Mote Plus" << endl;
-		}
-
-		if( mbuttons & MOTE_MINUS ) {
-			LOG_WII(DEBUG4) << "Mote Minus" << endl;
-		}
-
-		if( mbuttons & MOTE_HOME ) {
-			LOG(DEBUG4) << "Playing beep" << endl;
-			Beeper::beep(440,1000);
-		}
-
-		if( mbuttons & MOTE_A ) {
-			LOG_WII(DEBUG4) << "Mote A" << endl;
-		}
-
-		if( mbuttons & MOTE_B ) {
-			LOG_WII(DEBUG4) << "Mote B" << endl;
-		}
+		HumanInputInterface::instance()->setInputFlag();
 	}
 }
 
 void shutdown( int exitStatus ) {
 	LOG(INFO) << "Shutting down YClops" << endl;
 
-	LOG(INFO) << "Closing Wii Controller" << endl;
-	WiiController::destroyReference();
+	LOG(INFO) << "Closing Input Interface" << endl;
+	HumanInputInterface::destroyInterface();
 
+	//ai assumes yclops is still valid so it can't be deleted before ai
 	LOG(INFO) << "Deleting ai" << endl;
 	delete ai;	ai = NULL;
 
