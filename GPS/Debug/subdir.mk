9d50c4faf344a5988423f4e09f91efee48a4bca5
igvcbyu <igvcbyu@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-03-31 17:53:19 +0000

    continuing to write testcode

--- GPS/Debug/subdir.mk
+++ GPS/Debug/subdir.mk
@@ -4,15 +4,12 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../GPS2.cpp \
 ../main.cpp 
 
 OBJS += \
-./GPS2.o \
 ./main.o 
 
 CPP_DEPS += \
-./GPS2.d \
 ./main.d 
 
 
@@ -20,7 +17,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/home/igvcbyu/workspace/YClopsLib/inc" -I/usr/include/mrpt/base/include/ -I/usr/include/mrpt/mrpt-config/ -I/usr/include/mrpt/bayes/include/ -I/usr/include/mrpt/detectors/include/ -I/usr/include/mrpt/vision/include/ -I/usr/include/mrpt/maps/include/ -I/usr/include/mrpt/gui/include/ -I/usr/include/mrpt/slam/include/ -I/usr/include/mrpt/opengl/include/ -I/usr/include/mrpt/obs/include/ -I/usr/include/mrpt/scanmatching/include/ -I/usr/include/mrpt/hmtslam/include/ -I/usr/include/mrpt/hwdrivers/include/ -I/usr/include/mrpt/reactivenav/include/ -I/usr/include/mrpt/topography/include/ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -I"/home/igvcbyu/workspace/YClopsLib/inc" -I/usr/include/mrpt/base/include -I/usr/include/mrpt/mrpt-config -I/usr/include/mrpt/bayes/include -I/usr/include/mrpt/detectors/include -I/usr/include/mrpt/vision/include -I/usr/include/mrpt/maps/include -I/usr/include/mrpt/gui/include -I/usr/include/mrpt/slam/include -I/usr/include/mrpt/opengl/include -I/usr/include/mrpt/obs/include -I/usr/include/mrpt/scanmatching/include -I/usr/include/mrpt/hmtslam/include -I/usr/include/mrpt/hwdrivers/include -I/usr/include/mrpt/reactivenav/include -I/usr/include/mrpt/topography/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
