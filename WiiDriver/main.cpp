16899aecbc5309c526474c60c6127e99bcad3681
e2thenegpii <e2thenegpii@26bf7e6e-289a-d697-594d-b929f8fc7ac6>
2011-04-02 03:04:52 +0000

    Super update!!! Or like YClopsLib 2.0  Added a Model View Controller architecture to the project so we can change quickly both input and output interfaces.  Changed the sensors to interface with mrpt::hwdrivers::CGenericSensor.  (This is really good!!!)  It will allow us to more cleanly interface with mrpt.  Eldon

--- WiiDriver/main.cpp
+++ WiiDriver/main.cpp
@@ -12,20 +12,19 @@
 #include "MotorController.h"
 #include "MotorCommandInterface.h"
 #include "Compass.h"
-#include "HumanInputInterface.h"
 #include "logging.h"
 #include "YClopsConfiguration.h"
 #include "YClopsModel.h"
+#include "WiiController.h"
 
 #include <mrpt/hwdrivers/CSerialPort.h>
-
+#include "AbstractView.h"
+#include "NView.h"
 using namespace mrpt::utils;
 using namespace std;
 
 void signal_handler( int signum );
 void handleUserInput();
-
-extern YClopsModel * yclopsModel;
 
 ofstream of;
 const string DEFAULT_POINTS_FILE = "points.txt";
@@ -36,6 +35,11 @@ const string DEFAULT_POINTS_FILE = "points.txt";
  * @TODO make a human input hardware interface layer to remove the necessity of using the wii controller
  * @TODO create an serial port command interface to refactor the serial port sending and response sending
  */
+
+YClopsModel * yclopsModel = NULL;
+YClopsController * controller = NULL;
+MVC::AbstractView * console = NULL;
+
 int main( int argc, char** argv ) {
 
 	bool badClose = false;
@@ -61,24 +65,31 @@ int main( int argc, char** argv ) {
 		//Log::SetReportStreamBits(ALL_LOG);
 		Log::GetOStream()->precision(10);
 
-		//Set up the SIGUSR1 so we know when a button is pressed
-		LOG(DEBUG4) << "Initializing signals" << endl;
-		signal(SIGUSR1, signal_handler);
-		signal(SIGINT, signal_handler);
-
-		//Initialize the WiiController
-		LOG(DEBUG4) << "Creating the User Input Device" << endl;
-		HumanInputInterface::createWiiController();
-
 		LOG(DEBUG4) << "Creating YClops Model" << endl;
 		yclopsModel = new YClopsModel();
 
-		LOG(DEBUG4) << "Starting main loop" << endl;
-		while(!HumanInputInterface::getClosingFlag()) {
-			LOG(DEBUG4) << "Iterating: " << (!HumanInputInterface::getClosingFlag()?"true":"false") << endl;
+		LOG(DEBUG4) << "Setting up program view" << endl;
+		console = new MVC::ConsoleView();
+		//console = new NView(yclopsModel, NULL);
 
-			if(HumanInputInterface::instance()->getInputFlag())
-				HumanInputInterface::instance()->handleUserInput();
+		//The wii controller constructor needs a console to write to
+		//So this must be called after the console object is set up
+		LOG(DEBUG4) << "Creating the User Input Device" << endl;
+		//controller = new YClopsController(yclopsModel);
+		controller = new WiiController(yclopsModel,console);
+		console->setController(controller);
+
+		//Set up the SIGUSR1 so we know when a button is pressed
+		//this should be done after we set up the controller
+		//because the signal_handler uses the controller
+		LOG(DEBUG4) << "Initializing signals" << endl;
+		signal(SIGINT, signal_handler);
+
+		LOG(DEBUG4) << "Starting main loop" << endl;
+		while(!controller->getIsClosing()) {
+			LOG(DEBUG4) << "Iterating: " << (!controller->getIsClosing()?"true":"false") << endl;
+
+			controller->handleEvent();
 
 			switch(yclopsModel->ai->getCurrentState()) {
 			case mrpt::reactivenav::CAbstractReactiveNavigationSystem::IDLE:
@@ -100,16 +111,24 @@ int main( int argc, char** argv ) {
 			}
 			yclopsModel->ai->navigationStep();
 
+			//If we are using a wii motor command then we need to set the speeds ourselves
+			if(yclopsModel->yclops->isWiiMotorCommand()) {
+				double left, right;
+				controller->getRequestedSpeeds(left,right);
+				yclopsModel->yclops->changeSpeeds((uint16_t)left,(uint16_t)right);
+			}
+
 		}
 	} catch (...) {
 		LOG(ERROR) << "Unhandled Exception caught" << endl;
 		badClose = true; //We are closing with an error see the shutdown call below
 	}
 
-	HumanInputInterface::destroyInterface();
 	of.close();
 
-	delete yclopsModel;
+	delete controller;
+	delete yclopsModel;//Need to delete the console after the model because the model destructor uses logging which requires a view
+	delete console;
 
 	return (badClose?EXIT_FAILURE:EXIT_SUCCESS);
 }
@@ -118,8 +137,6 @@ void signal_handler( int signum ) {
 
 	if( SIGINT == signum ) {
 		LOG(DEBUG4) << " Signal Received " << signum << endl;
-		HumanInputInterface::setClosingFlag();
-	} else if( SIGUSR1 == signum ) {
-		HumanInputInterface::instance()->setInputFlag();
+		controller->setIsClosing();
 	}
 }
